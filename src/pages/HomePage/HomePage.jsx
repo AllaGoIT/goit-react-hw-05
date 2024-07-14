18dc3fc24813ffee8398c9b890a1ca249e706aed
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-07-14 16:01:36 +0300

    add code homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,20 @@
+import { useState, useEffect } from "react";
+import { fetchTrendingMovies } from "../../films-api";
 const HomePage = () => {
+  const [movie, setMovie] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchTrending() {
+      try {
+        setError(null);
+        await fetchTrendingMovies () {};
+      } catch {
+      } 
+    }
+    fetchTrending();
+  }, []);
+
   return <></>;
 };
 export default HomePage;
