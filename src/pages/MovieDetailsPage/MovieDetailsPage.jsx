f43859f2bb5c83ec4041ff288e988216947baa1c
AllaGoIT <153321544+AllaGoIT@users.noreply.github.com>
2024-07-17 20:01:29 +0300

    add style1

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react";
 import { Outlet, useParams } from "react-router-dom";
 import { fetchMovieDetails } from "../../films-api";
+import css from "./MovieDetailsPage.module.css";
+// import { useSearchParams } from "react-router-dom";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
-
   const [movies, setMovies] = useState(null);
   const [error, setError] = useState(false);
-
+  // const [searchParams] = useSearchParams();
+  // const genres = searchParams.get("name");
   useEffect(() => {
     async function fetchMoviesDetails() {
       try {
         setError(false);
-        const { results } = await fetchMovieDetails(movieId);
+        const results = await fetchMovieDetails(movieId);
         setMovies(results);
       } catch {
         setError(true);
@@ -23,14 +25,22 @@ const MovieDetailsPage = () => {
 
   return (
     <>
-      {movies > 0 && (
-        <div>
-          <h1>{movies.original_title}</h1>
-          <img src={movies.poster_path} />
-          <p>{movies.popularity}</p>
-          <p>Overview{movies.overview}</p>
-          <p>Genres{movies.genres.name}</p>
-          {error && <p>Opps! Error!</p>}
+      {movies && (
+        <div className={css.container}>
+          <img
+            className={css.img}
+            src={`https://image.tmdb.org/t/p/w500/${movies.poster_path}`}
+          />
+          <div className={css.containerText}>
+            <h1>{movies.original_title}</h1>
+            <p>User Score:{movies.popularity}</p>
+            <p className={css.textOver}>
+              <span className={css.overview}> Overview</span> <br />
+              {movies.overview}
+            </p>
+            <p>Genres{movies.genres.name}</p>
+            {error && <p>Opps! Error!</p>}
+          </div>
         </div>
       )}
       <Outlet />
